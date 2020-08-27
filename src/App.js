6acbc61f06a8142048e36764db52d46c231f327d
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-27 18:35:10 +0100

    Setup React-Router for page changes within app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./App.css";
+import Home from "./Home";
+import Header from "./Header";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
